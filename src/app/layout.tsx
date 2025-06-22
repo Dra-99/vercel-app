94e61186b205439813bf2359996f310ae9a9f7be
dra99 <wbl111@aliyun.com>
2025-06-22 18:49:07 +0800

    11

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Comforter } from "next/font/google";
+import { Comforter } from "next/font/google";
 import "./globals.css";
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 const comforter = Comforter({
   weight: '400',
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
